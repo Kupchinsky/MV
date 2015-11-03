eb795a5d1b6a9de189f000be449e5b46c2507cbd
Kupchinsky <user@killer666.ru>
2015-11-03 11:13:18 +0700

    - Lab3 last

--- Lab3/widget.h
+++ Lab3/widget.h
@@ -17,10 +17,6 @@ public:
     void resizeEvent (QResizeEvent*);
 
 private slots:
-    void on_pushButton_clicked();
-
-    void on_pushButton_2_clicked();
-
     void on_pushButtonLagra_clicked();
 
 private:
